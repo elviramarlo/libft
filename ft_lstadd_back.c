61d7a1eba5264e3ad9a14878936335e67f9728bd
elvmarti <elvmarti@student.42madrid.com>
2020-12-23 01:26:04 +0100

    libft

---
+++ ft_lstadd_back.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elvmarti <elvmarti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/08 11:12:49 by elvmarti          #+#    #+#             */
+/*   Updated: 2020/10/09 11:53:04 by elvmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list *str;
+
+	str = *lst;
+	if (!*lst)
+	{
+		*lst = new;
+	}
+	else
+	{
+		str = ft_lstlast(*lst);
+		str->next = new;
+	}
+}
